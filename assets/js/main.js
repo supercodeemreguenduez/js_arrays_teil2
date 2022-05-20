f4e518502d2999acffbe01dd97c3f4f91dbc8ba0
supercodemereguenduez <supercode.emreguenduez@gmail.com>
2022-05-20 09:08:50 +0200

    final

---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+// Array-Mathods Teil 2
+
+// Arrays2 1.1
+
+// let languages = [
+//     "JavaScript",
+//     "Python",
+//     "Java",
+//     "Ruby",
+//     "PHP",
+//     "C++",
+//     "CSS",
+//     "C#",
+//     "Go",
+//     "C",
+//     "TypeScript",
+//     "Swift"
+// ];
+
+// languages.sort();
+// console.log(languages);
+
+// 1.2
+
+// let languages = [
+//     "JavaScript",
+//     "Python",
+//     "Java",
+//     "Ruby",
+//     "PHP",
+//     "C++",
+//     "CSS",
+//     "C#",
+//     "Go",
+//     "C",
+//     "TypeScript",
+//     "Swift"
+// ];
+
+// let sortierung2 = languages.sort();
+
+// sortierung2.reverse();
+
+// console.log(sortierung2);
+
+// 2.2
+
+// let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+
+//l et numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+// numArray2.sort((a, b) => a - b);
+// console.log(numArray2);
+
+// 1.13
+
+// let meinText1 = ["Hello", "World"];
+// let meinText2 = ["Anass", "Elaine", "Eric", "Georg"];
+// let meinText3 = ["Superman", "Wonderwoman", "Hulk", "Batman", "Spiderman"];
+
+// let join1 = meinText1.join();
+// let join2 = meinText1.join("");
+// let join3 = meinText1.join(" ");
+// let join4 = meinText1.join("+");
+// let join5 = meinText2.join();
+// let join6 = meinText2.join("");
+// let join7 = meinText2.join(" ");
+// let join8 = meinText2.join("+");
+// let join9 = meinText3.join();
+// let join10 = meinText3.join("");
+// let join11 = meinText3.join(" ");
+// let join12 = meinText3.join("+");
+
+// console.log(join1);
+// console.log(join2);
+// console.log(join3);
+// console.log(join4);
+// console.log(join5);
+// console.log(join6);
+// console.log(join7);
+// console.log(join8);
+// console.log(join9);
+// console.log(join10);
+// console.log(join11);
+// console.log(join12);
+
+// 2.2
+
+// let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+
+// numArray2.sort((a, b) => a - b);
+// console.log(numArray2);
+
